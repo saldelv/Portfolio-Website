88aa8f918850075f0fe7a6f879b14ac601706370
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-07-07 20:12:46 -0400

    Theme color buttons

--- src/components/themes.js
+++ src/components/themes.js
@@ -1,17 +1,50 @@
 import React, { useState } from 'react'
 import '../styles/themes.css'
 
+const themeColors = [
+    {
+        color: '#667db8'
+    },
+    {
+        color: 'white'
+    },
+    {
+        color: '#515151'
+    },
+    {
+        color: '#66b883'
+    },
+    {
+        color: '#b95252'
+    },
+    {
+        color: '#9966b8'
+    }
+]
+
 function Themes() {
 
     const [open, setOpen] = useState(false)
 
     return (
         <div className='themes' id={open ? "open" : "close"}>
-            <button onClick={() => setOpen(!open)}>
-                <p>Themes</p>
-            </button>
+            <div className = "barButton">
+                <button onClick={() => setOpen(!open)}>
+                    <p>Themes</p>
+                </button>
+            </div>
             <div className='openThemes'>
-                opened
+                <div className='grid'>
+                    {themeColors.map((themeColor, key) => {
+                        return (
+                        <div className="color">
+                            <button style={{backgroundColor:`${themeColor.color}`}}>
+                                <p>color</p>
+                            </button>
+                        </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     );
